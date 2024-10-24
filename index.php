b2613bd98cac699e7da2e532d4e83444dcfe3dda
Tomasz Semla <EMAIL>
2024-10-24 08:41:52 +0200

    Wyświetlanie danych php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$servername = "localhost";
+$username = "root";
+$password ="";
+$dbname ="p_edu";
+$conn = mysli_connect($servername, $username, $password, $dbname);
+if(!$conn){
+    die("błąd połączenia:" mysqli_connect_error());
+}
+$sql = "select * from uczniowie";
+$result = mysqli_query($conn, $sql);
+if(mysqli_num_rows($result) >0){
+    echo "<table border='1'><tr><th>Imię</th><th>Nazwisko</th><th>Wiek</th></tr>";
+while($row = mysqli_fetch_assoc($result)){
+    echo "<tr><td>".$row["imie"]."</td><td>".$row["nazwisko"]."</td><td>".$row["wiek"]."</td></tr>"
+}
+echo "</table>";
+}
+else {
+    echo "Brak wyników";
+}
+mysqli_close($conn);
+    ?>
+</body>
+</html>
